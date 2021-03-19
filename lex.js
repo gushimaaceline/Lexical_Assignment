715004ef7b41e815060a188151e982d1e9af3ea9
gushimaaceline <celinegushimaa@gmail.com>
2021-03-19 22:30:16 +0300

    first commit

---
+++ lex.js
@@ -0,0 +1,10 @@
+function freinds() {
+    var x="Juliet";
+    var y="Sarah";
+    function fact() {
+        var z= x+ " and " +y+ " are freinds";
+        console.log(z);
+    }
+   fact();
+}
+freinds();
